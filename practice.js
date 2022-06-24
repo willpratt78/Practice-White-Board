79400893ad0f86d61f59424c4c9962fef7281d29
William Pratt <willpratt78@gmail.com>
2022-06-24 08:50:43 -0700

    first prompt worked through

---
+++ practice.js
@@ -0,0 +1,17 @@
+Prompt 1
+
+URLs cannot have spaces. Instead, all spaces in a string are replaced with %20. Write an algorithm that replaces all spaces in a string with %20.
+
+You may not use the replace() method or regular expressions to solve this problem. Solve the problem with and without recursion.
+
+Example
+Input: "Jasmine Ann Jones"
+
+Output: "Jasmine%20Ann%20Jones"
+
+
+element = " "
+Input.forEach {
+ if (element == true)
+    element = "%20"
+};
