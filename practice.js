ea86073a2868c02f7b34448d8d85a723f9137c12
William Pratt <willpratt78@gmail.com>
2022-06-24 11:45:44 -0700

    prompt 5 worked through

--- practice.js
+++ practice.js
@@ -68,3 +68,80 @@ Input.forEach () {
   else
     return true
 }
+
+
+
+prompt 5
+
+Write an algorithm that sorts an array without using the sort() method. There are many different sorting algorithms - take the time to read about the following:
+
+Quick sort
+Merge sort
+Heap sort
+Insertion sort
+Bubble sort
+Selection sort
+You may implement any of the above algorithms (or your own) to solve the problem - as long as it doesn't use sort().
+
+Example
+Input: [9, 2, 7, 12]
+
+Output: [2, 7, 9, 12]
+
+import java.io.*;
+ 
+class GFG{
+     
+static void swap(int[] arr, int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+ 
+static int partition(int[] arr, int low, int high)
+{
+     
+    int pivot = arr[high];
+     
+    int i = (low - 1);
+ 
+    for(int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, high);
+    return (i + 1);
+}
+ 
+static void quickSort(int[] arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+ static void printArray(int[] arr, int size)
+{
+    for(int i = 0; i < size; i++)
+        System.out.print(arr[i] + " ");
+         
+    System.out.println();
+}
+ 
+public static void main(String[] args)
+{
+    int[] arr = { 10, 7, 8, 9, 1, 5 };
+    int n = arr.length;
+     
+    quickSort(arr, 0, n - 1);
+    System.out.println("Sorted array: ");
+    printArray(arr, n);
+}
+}
