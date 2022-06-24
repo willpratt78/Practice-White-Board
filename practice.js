aac963a4ec5265babe395c6221bcefbedea9268b
William Pratt <willpratt78@gmail.com>
2022-06-24 10:31:25 -0700

    prompt 3 worked through

--- practice.js
+++ practice.js
@@ -34,3 +34,19 @@ Input.forEach {
     }
     return Input
 }
+
+prompt 3
+
+Write an algorithm that takes a string with repeated characters and compresses them, using a number to show how many times the repeated character has been compressed. For instance, aaa would be written as 3a. Solve the problem with and without recursion.
+
+Example
+Input: "aaabccdddda"
+
+Output: "3ab2c4da"
+
+Input.forEach {
+  if(array2.includes(Input))
+    array2[i-1] += 1
+  else
+    push.(array2)
+};
